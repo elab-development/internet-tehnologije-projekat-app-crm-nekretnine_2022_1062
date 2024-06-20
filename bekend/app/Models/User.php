f9ab6aa67c5a32f0382d3238ebae214e2615a3c8
Maxim15420 <materijalizaucenje11@gmail.com>
2024-06-20 23:49:46 +0200

    kreiran model aplikacije

--- bekend/app/Models/User.php
+++ bekend/app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
