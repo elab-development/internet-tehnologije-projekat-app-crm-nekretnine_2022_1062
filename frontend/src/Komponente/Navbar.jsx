b660dc52f2cc010e6ae284d62b70b58617cddc53
viktorm10 <viktor.marcic011@gmail.com>
2024-06-23 21:55:46 +0200

    testiranje

--- frontend/src/Komponente/Navbar.jsx
+++ frontend/src/Komponente/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = ({ token, setToken }) => {
         ) : (
           <>
             <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
+            
           </>
         )}
       </ul>
